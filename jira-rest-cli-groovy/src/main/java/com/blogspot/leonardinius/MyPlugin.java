3a9a528bf0c903e826ba3db75ef7a5a88a2d6b5a
Leonid Maslov <leonid.maslov@sonyericsson.com>
2011-03-12 12:31:26 +0100

    Initial project skeleton. Lot's to follow

---
+++ jira-rest-cli-groovy/src/main/java/com/blogspot/leonardinius/MyPlugin.java
@@ -0,0 +1,5 @@
+package com.blogspot.leonardinius;
+
+public class MyPlugin
+{
+}
---
+++ jira-rest-cli-groovy/src/test/java/com/blogspot/leonardinius/MyPluginTest.java
@@ -0,0 +1,11 @@
+package com.blogspot.leonardinius;
+
+import org.junit.Test;
+
+public class MyPluginTest
+{
+    @Test
+    public void testSomething()
+    {
+    }
+}
