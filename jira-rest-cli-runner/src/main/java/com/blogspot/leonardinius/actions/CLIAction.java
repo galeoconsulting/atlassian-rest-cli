db2dfa9b69aa949b0da6a58f6cc7baa2b4ecef4f
Leonid M <leonidms@gmail.com>
2011-03-13 12:51:35 +0100

    jquery-console initiated

---
+++ jira-rest-cli-runner/src/main/java/com/blogspot/leonardinius/actions/CLIAction.java
@@ -0,0 +1,12 @@
+package com.blogspot.leonardinius.actions;
+
+import com.atlassian.jira.web.action.JiraWebActionSupport;
+
+/**
+ * User: leonidmaslov
+ * Date: 3/13/11
+ * Time: 11:34 AM
+ */
+public class CLIAction extends JiraWebActionSupport
+{
+}
