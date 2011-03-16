9eb569883cbfad5c887d117cbe53af44fa9b5500
Leonid M <leonidms@gmail.com>
2011-03-16 23:27:55 +0100

    cli-remote script updated - uses readline if possible - allows multiline inputs (no validation and no intellectual stuff though)

--- examples/cli-remote.rb
+++ examples/cli-remote.rb
@@ -2,70 +2,124 @@ require 'rubygems'
 require 'restclient'
 require 'json'
 
-def from_json(str)
-    JSON::parse str
-end
+module RESTCli
+    class Cli
+        public
 
-(proto, host, port, context) = ["http", "localhost", 2990, "jira/"]
-(username, password) = ["admin", "admin"]
+        attr_accessor :options
 
-AUTH_BASE_URL = "#{proto}://#{host}:#{port}/#{context}rest/auth/latest/session"
-def login(username, password)
-    logindata = RestClient.post(AUTH_BASE_URL, {"username" => username, "password" => password}.to_json, { :content_type => :json,
-    :accept => :json })
-    from_json(logindata)["session"]
-end
-
-CLI_BASE_URL = "#{proto}://#{host}:#{port}/#{context}rest/rest-scripting/1.0"
-OPTS = { :content_type => :json, :accept => :json }
-
-def newSessionId
-    sessionId = RestClient.put("#{CLI_BASE_URL}/sessions", {"language" => "ruby"}.to_json, OPTS)
-    from_json(sessionId)["sessionId"]
-end
-
-def deleteSession(sessionId)
-    raise StandardError, "SessionId should be specified. Empty value passed in" if sessionId.nil? or sessionId == ""
-    RestClient.delete("#{CLI_BASE_URL}/sessions/#{sessionId}", OPTS)
-end
-
-def cli(sessionId, script)
-    evalResult = RestClient.post("#{CLI_BASE_URL}/sessions/#{sessionId}", {"script" => script}.to_json, OPTS)
-    from_json(evalResult)
-end
-
-def listSessions
-    sessions = RestClient.get("#{CLI_BASE_URL}/sessions", OPTS)
-    from_json(sessions)["sessions"].select{|i| i["languageName"]["Ruby"] }.map{ |e| e["sessionId"]}
-end
-
-def repl(sessionId = nil, killSessionOnExit = true)
-    sessionId ||= newSessionId()
-    begin
-        puts " ***************************************** "
-        puts " **  JIRA REST-Ruby-Cli command line    ** "
-        puts " **  Type 'quit' to exit the loop       ** "
-        puts " ***************************************** "
-        while true
-            $stdout.print "JIRA-CLI>> "
-            input = gets()
-            case input.strip
-            when "quit" then break;
-            else
-                out = cli(sessionId, input)
-            end
-            $stdout.puts  out["out"] unless out["out"] == ""
-            $stderr.puts  out["err"] unless out["err"] == ""
-            $stdout.print "JIRA-CLI=> ", out["evalResult"], "\n" 
+        def initialize(options)
+            @options = options || {}
+            @sessionOpts = { :content_type => :json, :accept => :json }
         end
-    ensure
-        deleteSession(sessionId) unless sessionId.nil? and not killSessionOnExit
+
+        def loginUrl
+            "#{@options[:proto]}://#{@options[:host]}:#{@options[:port]}/#{@options[:context]}rest/auth/latest/session"
+        end
+
+        def cliBaseUrl
+            "#{@options[:proto]}://#{@options[:host]}:#{@options[:port]}/#{@options[:context]}rest/rest-scripting/1.0"
+        end
+
+        def login(username, password)
+            logindata = RestClient.post(loginUrl, {"username" => username, "password" => password}.to_json, { :content_type => :json,
+            :accept => :json })
+            session = from_json(logindata)["session"]
+            @sessionOpts.merge!({:cookies => {session["name"] => session["value"] } })
+        end
+
+        def newSessionId
+            sessionId = RestClient.put("#{cliBaseUrl}/sessions", {"language" => "ruby"}.to_json, sessionOpts)
+            from_json(sessionId)["sessionId"]
+        end
+
+        def deleteSession(sessionId)
+            raise StandardError, "SessionId should be specified. Empty value passed in" if sessionId.nil? or sessionId == ""
+            RestClient.delete("#{cliBaseUrl}/sessions/#{sessionId}", sessionOpts)
+        end
+
+        def eval_input(sessionId, script)
+            evalResult = RestClient.post("#{cliBaseUrl}/sessions/#{sessionId}", {"script" => script}.to_json, sessionOpts)
+            from_json(evalResult)
+        end
+
+        def listSessions
+            sessions = RestClient.get("#{cliBaseUrl}/sessions", sessionOpts)
+            from_json(sessions)["sessions"].select{|i| i["languageName"].downcase["ruby"] }.map{ |e| e["sessionId"]}
+        end
+
+        def repl(sessionId = nil, killSessionOnExit = true)
+            puts " ***************************************** "
+            puts " **  JIRA REST-Ruby-Cli command line    ** "
+            puts " **  Type '!q' to exit the loop         ** "
+            puts " **  Type '!r' to eval user input       ** "
+            puts " ***************************************** "
+            sessionId ||= (
+                tmp = newSessionId()
+                $stdout.puts "New session acquired: #{tmp}";
+                tmp
+                )
+            begin
+                while true
+                    case stdinGets.strip
+                    when "!q" then break;
+                    when "!r" then
+                        out = eval_input(sessionId, stdinReset)
+                        $stdout.puts  out["out"] unless out["out"] == ""
+                        $stderr.puts  out["err"] unless out["err"] == ""
+                        $stdout.print "JIRA-CLI=> ", out["evalResult"], "\n"
+                    end
+                end
+            ensure
+                deleteSession(sessionId) unless sessionId.nil? and not killSessionOnExit
+            end
+        end
+
+        private
+        def from_json(str)
+            JSON::parse str
+        end
+
+        attr_accessor :sessionOpts
+
+        def stdin
+            @io ||= (
+            require 'irb'
+            @io = IRB::ReadlineInputMethod.new if defined? IRB::ReadlineInputMethod
+            @io ||= IRB::StdioInputMethod.new
+            update_prompt(@io)
+            )
+        end
+
+        def stdinGets
+            s = stdin.gets
+            update_prompt(stdin)
+            s
+        end
+
+        def stdinReset
+            text = stdin.instance_variable_get('@line')
+            stdin.instance_variable_set('@line_no', 0)
+            stdin.instance_variable_set('@line', [])
+            update_prompt(stdin)
+            text.pop
+            ##pp text
+            text.join('')
+        end
+
+        def update_prompt(io)
+            s = io.instance_variable_get('@line_no').to_s
+            io.prompt= "JIRA-CLI(#{"0" * (5-s.length)}#{s})>> "
+            io
+        end
     end
 end
 
+cli = RESTCli::Cli.new({:proto => 'http',
+                        :host => 'localhost',
+                        :port => 2990,
+                        :context => 'jira/'})
 
-authData = login(username, password)
-OPTS[:cookies] = {authData["name"] => authData["value"] }
-repl()
-
-
+cli.login('admin', 'admin')
+puts "Active ruby sessions: " + cli.listSessions.join(', ')
+cli.repl()
