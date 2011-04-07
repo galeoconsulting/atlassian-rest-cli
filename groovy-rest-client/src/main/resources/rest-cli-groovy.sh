55697bc23f6c3c7c10cda31ae886f2934e1211bc
Leonid M <leonidms@gmail.com>
2011-04-07 21:41:11 +0200

    Fixed some git location mis-location

---
+++ groovy-rest-client/src/main/resources/rest-cli-groovy.sh
@@ -0,0 +1,18 @@
+#! /bin/bash
+
+SCRIPT_LOCATION=$0
+# Step through symlinks to find where the script really is
+while [ -L "$SCRIPT_LOCATION" ]; do
+  SCRIPT_LOCATION=`readlink -e "$SCRIPT_LOCATION"`
+done
+
+BIN_HOME=`dirname "$SCRIPT_LOCATION"`
+LIB_HOME=$BIN_HOME/../lib
+
+CLASSPATH=$BIN_HOME/${pom.artifactId}-${pom.version}.jar
+for i in `ls $LIB_HOME/*.jar`;
+do
+    CLASSPATH=$CLASSPATH:$LIB_HOME/$i
+done
+
+java -cp $CLASSPATH RestCli $*
