661fa00f969ace657d64f3db798313095119bd73
Leonid M <leonidms@gmail.com>
2011-04-09 00:46:37 +0200

    Initial POC of configuration automation

--- groovy-rest-client/src/main/groovy/RestCli.groovy
+++ groovy-rest-client/src/main/groovy/RestCli.groovy
@@ -14,12 +14,6 @@
  * limitations under the License.
  */
 
-
-
-
-
-
-
 import com.sun.jersey.api.client.Client
 import com.sun.jersey.api.client.ClientResponse
 import com.sun.jersey.api.client.config.DefaultClientConfig
